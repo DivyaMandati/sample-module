cb1131d481527737af2d5d6876bcc7cbbffde0f5
SaiSaketh243 <sai.saketh1992@outlook.com>
2024-09-19 12:03:25 +0530

    Create main.tf

---
+++ storage-account-module/main.tf
@@ -0,0 +1,9 @@
+# This is the module for Azure Storage Account
+resource "azurerm_storage_account" "storage_account" {
+  name                     = local.storage_account_name
+  resource_group_name       = var.resource_group_name
+  location                  = var.location
+  account_tier              = var.account_tier
+  account_replication_type  = var.account_replication_type
+  tags                      = var.tags
+}
