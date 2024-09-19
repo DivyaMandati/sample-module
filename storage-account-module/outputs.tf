25cc08edb4037a641387f2aadefc2444bf25d4c8
SaiSaketh243 <sai.saketh1992@outlook.com>
2024-09-19 12:04:57 +0530

    Create outputs.tf

---
+++ storage-account-module/outputs.tf
@@ -0,0 +1,9 @@
+output "storage_account_id" {
+  description = "The ID of the storage account."
+  value       = azurerm_storage_account.storage_account.id
+}
+
+output "storage_account_primary_endpoint" {
+  description = "The primary endpoint of the storage account."
+  value       = azurerm_storage_account.storage_account.primary_blob_endpoint
+}
