b2da2decb33583dff33ef76de7e3f0dc8b44e8ab
SaiSaketh243 <sai.saketh1992@outlook.com>
2024-10-09 08:42:36 +0530

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,8 +1,18 @@
+#locals {
+#  # exmaple names on how they look, 
+#  # rg-sas-psu-dev,  rg-sas-psu-qa,  rg-sas-psu-stg
+#  # rg-sas-nrm-dev, rg-sas-nrm-stg, rg-sas-nrm-prod
+#  prefix = "rg"
+#  name   = "${local.prefix}-${var.main_project}-${var.sub_project}-${var.environment}"
+# }
+
 locals {
-  # exmaple names on how they look, 
-  # rg-sas-psu-dev,  rg-sas-psu-qa,  rg-sas-psu-stg
-  # rg-sas-nrm-dev, rg-sas-nrm-stg, rg-sas-nrm-prod
+  # Resource group naming convention
   prefix = "rg"
   name   = "${local.prefix}-${var.main_project}-${var.sub_project}-${var.environment}"
+
+  # Storage account naming convention
+  storage_account_name = "st${var.main_project}${var.sub_project}${var.environment}${random_id.random_hex.hex}"
 }
+
 
